e63021020f673e632366ecfc68140fc33273da10
Kilokiyiu <kilokiyiu0916@gmail.com>
2026-04-21 15:04:27 +0800

    优化

--- MiniWeb API/MiniWeb_Middleware/PathParser.cs
+++ MiniWeb API/MiniWeb_Middleware/PathParser.cs
@@ -5,15 +5,7 @@ namespace MiniWebAPI
 {
     public class PathParser
     {
-        /// <summary>
-        /// 解析出来控制器和Action的名字。
-        /// 从请求路径中分析出来控制器的名字和操作方法的名字，
-        /// 如果路径分析失败，返回值中的ok的值为false，
-        /// 如果请求路径为”/Test1/Save”，
-        /// 则Parse方法的返回值为(true,”Test1”,”Save”)；
-        /// </summary>
-        /// <param name="pathString"></param>
-        /// <returns></returns>
+
         public static (bool ok,string? controllerName,string? actionName) Parse(PathString pathString)
         {
             string? path = pathString.Value;
@@ -21,7 +13,6 @@ namespace MiniWebAPI
             {
                 return (false, null, null);
             }
-            //解析出来控制器和Action的名字
             var match = Regex.Match(path,
                 "/([a-zA-Z0-9]+)/([a-zA-Z0-9]+)");
             if (!match.Success)
